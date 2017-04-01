9dd9022aeca30dcc682325c5102b164c6bdae729
Devang Bhuva <devangbhuva123@yahoo.com>
2017-04-01 12:05:03 +0530

    desktop view slider complete

--- js/slider.js
+++ js/slider.js
@@ -1,30 +1,32 @@
-$('.tile').mouseover(function() {
+
+$('.tile').each(function() {
     var hoverid = this.id;
-    console.log(hoverid);
 
     var tiles = document.getElementsByClassName('tile');
 
     var hoverTile = document.getElementById(hoverid);
 
+    var id = parseInt(hoverid) + 4;
+
     hoverTile.addEventListener('mouseover', function() {
-      for (var i = 0; i < (hoverid-1); i++) {
-        var tile = tiles[i];
+      for (var i = 0; i < id; i++) {
+        var tile = tiles[i];      
         tile.classList.add('shiftLeft');
       }
       
-      for (var i = hoverid; i < tiles.length; i++) {
+      for (var i = id; i < tiles.length; i++) {
         var tile = tiles[i];
         tile.classList.add('shiftRight');
       }
     });
 
     hoverTile.addEventListener('mouseout', function() {
-      for (var i = 0; i < (hoverid-1); i++) {
+      for (var i = 0; i < id; i++) {
         var tile = tiles[i];
         tile.classList.remove('shiftLeft');
       }
       
-      for (var i = hoverid; i < tiles.length; i++) {
+      for (var i = id; i < tiles.length; i++) {
         var tile = tiles[i];
         tile.classList.remove('shiftRight');
       }
@@ -36,45 +38,19 @@ $('.responsive').slick({
   dots: true,
 	prevArrow: $('.prev'),
 	nextArrow: $('.next'),
-  infinite: false,
-  // autoplay: true,
+  infinite: true,
+  autoplay: true,
   dots: false,
   speed: 1000,
   slidesToShow: 5,
-  slidesToScroll: 1,
+  slidesToScroll: 4,
   responsive: [
     {
       breakpoint: 1331,
       settings: {
         slidesToShow: 4,
-        slidesToScroll: 1,
-        infinite: false,
+        slidesToScroll: 3,
         dots: false,
-      }
-    },
-    {
-      breakpoint: 1101,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        infinite: false,
-        dots: false,
-      }
-    },
-    {
-      breakpoint: 851,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 1,
-        infinite: false,
-        dots: false,
-      }
-    },
-    {
-      breakpoint: 601,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1
       }
     }
   ]
